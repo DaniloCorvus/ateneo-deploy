4a5138db7311aee284cfb630ed51cb92e94c456b
Roberth <robinmoralesquiroz@gmail.com>
2022-01-13 11:50:51 -0500

    actualizacion omdelo productoDtoationType

--- main/app/Models/ProductDotationType.php
+++ main/app/Models/ProductDotationType.php
@@ -2,17 +2,11 @@
 
 namespace App\Models;
 
-<<<<<<< HEAD
-=======
 use Illuminate\Database\Eloquent\Factories\HasFactory;
->>>>>>> cad4ec93f5b5bac9371b86520ae0a632e88ced6e
 use Illuminate\Database\Eloquent\Model;
 
 class ProductDotationType extends Model
 {
-<<<<<<< HEAD
-    //
-=======
 
 
     protected $fillable = ['name'];
@@ -21,5 +15,4 @@ class ProductDotationType extends Model
     public function inventary(){
         return $this->hasMany(InventaryDotation::class)->where('stock','>','0');
     }
->>>>>>> cad4ec93f5b5bac9371b86520ae0a632e88ced6e
 }
